05d60b62d57544a1efa398b7b726dbbff691ad43
umerb2410 <110354270+umerb2410@users.noreply.github.com>
2022-08-10 13:03:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_iam_user" "iamuser1" {
 resource "aws_iam_user" "iamuser2" {
   name = var.username2
 }
+
+resource "aws_iam_user" "iamuser3" {
+  name = var.username3
+}
+
+resource "aws_iam_user" "iamuser4" {
+  name = var.username4
+}
